93fcd4abe3399d02ee1fae882e9dbc8ce81bca56
nakzz <naqibprio10@gmail.com>
2018-02-13 18:24:19 -0600

    Vertical Navbar updated

--- genetics_stocks.php
+++ genetics_stocks.php
@@ -38,27 +38,28 @@
                   <article class="item">
                     <div class="mb-thumb">
                       <img src="img/blog/1-big.jpg" class="img-responsive" alt=""/>
-                      
+
                     </div>
                     <!-- #BeginEditable "PageContent" -->
-                    <h4>FPsc Genetics Stocks</h4>
+                    <h2>FPsc Genetics Stocks</h2>
                     <p>FPsc was derived from the Wisconsin Fast Plants (WFP) variety of rapid-cycling <em>Brassica rapa</em>. The WFP variety was developed at UW-Madison as a model organism &#8212; it grows rapidly and completes its lifecycle in about 5 weeks. Because of its fast-growing nature, WFP has been used for years to model plant growth in classrooms.</p>
                     <p>FPsc was developed after extensive inbreeding of the WFP line, resulting in plants that show homozygous alleles at essentially all loci and have lost the mechanism of self-incompatibility.<img name="placeholder1" src="" width="150" height="150" alt="" align="right"/>  </p>
-                    <h2 id="wild">Wild-type  </h2>
+                    <h4 id="wild">Wild-type  </h4>
                     <p>Because of its extensive inbreeding and allele homozygosity, FPsc phenotype and PCR-products show little variance. This genetic uniformity leads to a stronger correlation of </p>
-                    <p>Mutagenesis was induced in FPsc plantsThrough chemical mutagenesis, several FPsc mutant strains have been developed, including:</p>
-                    <ul><li>Albino</li>
-                    <li>Dwarf</li>
-                    <li>Curled leaf</li>
-                    </ul>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis ligula lorem, consequat eget, tristique nec, auctor quis, purus. Vivamus ut sem. Fusce aliquam nunc vitae purus. Aenean viverra malesuada libero. Fusce ac quam. Donec neque. Nunc venenatis enim nec quam. Cras faucibus, justo vel accumsan aliquam, tellus dui fringilla quam, in condimentum augue lorem non tellus. Pellentesque id arcu non sem placerat iaculis. Curabitur posuere, pede vitae lacinia accumsan, enim nibh elementum orci, ut volutpat eros sapien nec sapien. Suspendisse neque arcu, ultrices commodo, pellentesque sit amet, ultricies ut, ipsum. Mauris et eros eget erat dapibus mollis. Mauris laoreet posuere odio. Nam ipsum ligula, ullamcorper eu, fringilla at, lacinia ut, augue. Nullam nunc.</p>
-                    <p>Sed et lectus in massa imperdiet tincidunt. Praesent neque tortor, sollicitudin non, euismod a, adipiscing a, est. Mauris diam metus, varius nec, faucibus at, faucibus sollicitudin, lectus. Nam posuere felis ac urna. Vestibulum tempor vestibulum urna. Nullam metus. Vivamus ac purus. Nullam interdum ullamcorper libero. Morbi vehicula imperdiet justo. Etiam mollis fringilla ante. Donec et dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Etiam mi libero, luctus nec, blandit ac, rutrum ac, lectus.</p>
-                    <p>Morbi consequat felis vitae enim. Nunc nec lacus. Vestibulum odio. Morbi egestas, urna et mollis bibendum, enim tellus posuere justo, eget elementum purus urna nec lacus. Nullam in nulla. Praesent ac lorem. Donec metus risus, accumsan ut, mollis non, porttitor eget, mi. Aliquam aliquet, tortor a elementum aliquam, erat odio sodales eros, suscipit blandit lectus dolor sit amet elit. In eros wisi, mollis vitae, tincidunt in, suscipit id, nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus ornare. Suspendisse potenti. Mauris convallis. Vestibulum nec mauris in augue porta mollis. Proin ut nunc. Mauris aliquam dui eget purus.</p>
-                    <h2>R500</h2>
+                    <p>Mutagenesis was induced in FPsc plants through chemical mutagenesis, several FPsc mutant strains have been developed, including:</p>
+
+                    <div class="col-md-12">
+                      <div class="col-md-3 col-md-offset-5 ">
+                      <p>Albino <br> Dwarf <br> Curled Leaf <br></p>
+                      </div>
+                    </div>
+
+                    <h4>R500</h4>
                     <p>R500 is a strain of <em>B. rapa</em> grown primarily in India for the oil in its seeds. It is a naturally-selfing variety, and has developed significant uniformity in its genome.</p>
                     <p>R500 is used as an outcross for FPsc. The F1 generation resulting from the R500xFPsc cross will be heterozygous at all loci. The F2 generation, produced from a self-pollination of the F1 generation, will have a mixture of genes from FPsc and R500 that may be homozygous or heterozygous at any loci (however, the genetic makeup of the F2 generation is still 50 percent FPsc and 50 percent R500).</p>
                     <p>When genetic material from the FPsc x R500 F2 generation is subjected to PCR and gel electrophoresis, a mixture of bands associated with FPsc and R500 will be present.</p>
-                    <h2 id="mutant">Mutant-types</h2>
+                    <h4 id="mutant">Mutant-types</h4>
+                    <p>Updating...</p>
                     <p>&nbsp;</p>
                     <p>&nbsp;</p><!-- #EndEditable -->
 
